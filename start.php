8c3eb03ecea70352af9501ac406c768fea93f48d
Stevie <yzm629@sina.cn>
2021-07-19 12:36:26 +0800

    init

---
+++ start.php
@@ -0,0 +1,63 @@
+<?php
+require 'vendor/autoload.php';
+
+use Mark\App;
+
+use Workerman\Protocols\Http\{Request, Response};
+
+use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelHigh;
+use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeMargin;
+use Endroid\QrCode\Writer\SvgWriter;
+
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Symfony\Contracts\Cache\ItemInterface;
+
+$cache = new FilesystemAdapter('', 0, '/tmp/mark_cache');
+
+App::$pidFile = '/tmp/mark-run.pid';
+App::$logFile = '/tmp/mark-run.log';
+
+$api = new App('http://0.0.0.0:8787');
+
+$api->count = 2; // process count
+
+$api->get('/', function () {
+    return 'Hello world';
+});
+
+$api->get('/qrcode', function (Request $requst) use ($cache){
+    $get = new \stdClass();
+    $get->data = $requst->get('data') ?? 'xxxxx';
+    $get->size = (int)($requst->get('size') ?? 300);
+
+    $cache_key = md5('qrcode'.implode(',', (array)$get));
+
+    $cache->delete($cache_key);
+    $result = $cache->get($cache_key, function (ItemInterface $item) use ($get) {
+        $item->expiresAfter(60 * 60);
+
+        $result = Builder::create()
+            ->writer(new SvgWriter())
+            ->writerOptions([])
+            ->data($get->data)
+            ->encoding(new Encoding('UTF-8'))
+            ->errorCorrectionLevel(new ErrorCorrectionLevelHigh())
+            ->size($get->size)
+            ->margin(10)
+            ->roundBlockSizeMode(new RoundBlockSizeModeMargin())
+            ->build();
+
+        return [
+            'mime_type' => $result->getMimeType(),
+            'string' =>  $result->getString()
+        ];
+    });
+
+    return new Response(200, [
+        'Content-Type' => $result['mime_type']
+    ], $result['string']);
+});
+
+$api->start();
