6fdfb0a019cc84ad31cd955239f0f8b66acd6445
Stevie <yzm629@sina.cn>
2021-07-19 15:25:38 +0800

    update

--- start.php
+++ start.php
@@ -10,6 +10,7 @@ use Endroid\QrCode\Encoding\Encoding;
 use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelHigh;
 use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeMargin;
 use Endroid\QrCode\Writer\SvgWriter;
+use Endroid\QrCode\Writer\PngWriter;
 
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Contracts\Cache\ItemInterface;
@@ -31,15 +32,18 @@ $api->get('/qrcode', function (Request $requst) use ($cache){
     $get = new \stdClass();
     $get->data = $requst->get('data') ?? 'xxxxx';
     $get->size = (int)($requst->get('size') ?? 300);
+    $get->ext = $requst->get('ext') ?? 'svg';
 
     $cache_key = md5('qrcode'.implode(',', (array)$get));
 
-    $cache->delete($cache_key);
+    // debug delete cache
+    // $cache->delete($cache_key);
+    
     $result = $cache->get($cache_key, function (ItemInterface $item) use ($get) {
         $item->expiresAfter(60 * 60);
 
         $result = Builder::create()
-            ->writer(new SvgWriter())
+            ->writer($get->ext == 'png' ? new PngWriter() : new SvgWriter())
             ->writerOptions([])
             ->data($get->data)
             ->encoding(new Encoding('UTF-8'))
