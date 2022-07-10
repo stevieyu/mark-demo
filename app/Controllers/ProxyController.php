ffcdd3680098ecc1c570105f6fd957e50834638f
Stevie <yzm629@sina.cn>
2022-07-11 02:53:06 +0800

    feat: proxy for image

--- app/Controllers/ProxyController.php
+++ app/Controllers/ProxyController.php
@@ -3,13 +3,18 @@
 namespace App\Controllers;
 
 use Workerman\Protocols\Http\Request;
+use Workerman\Protocols\Http\Response;
 use GuzzleHttp\Client;
 
 class ProxyController extends BaseController
 {
-    public function index(Request $request): string
+    public function index(Request $request): Response
     {
         $r = $request->get('r', 'http://httpbin.org/anything?r=x');
-        return (new Client())->get($r)->getBody()->getContents();
+        $res = (new Client())->get($r);
+
+        return new Response(200, [
+            'Content-Type' => implode('', $res->getHeader('Content-Type'))
+        ], $res->getBody()->getContents());
     }
 }
